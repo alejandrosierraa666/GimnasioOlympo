6923ad48b3dc635851efa8948819de7c79e5306a
Alejandro Sierra <alejandro.sierralagartera@riberadeltajo.es>
2025-12-16 18:09:44 +0100

    First login done!

--- GimnasioOlympo/pages/login.php
+++ GimnasioOlympo/pages/login.php
@@ -1,3 +1,15 @@
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if(!isset($_POST['user']) || !isset($_POST['password']) || empty($_POST['user']) || empty($_POST['password'])) return;
+        include('../db/db.php');
+        
+        $result = $bd->query('select * from users where user like ? and password like ?', [$_POST['user'], password_hash($_POST['password'], PASSWORD_BCRYPT)]);
+
+        if(!$result) return;
+        else header('Location: ../index.php');
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +18,21 @@
     <title>Login</title>
 </head>
 <body>
-    
+    <main class="container">
+        <h1>Inicia Sesión</h1>
+
+        <section class="form__container">
+            <form action="" method="POST">
+                <label for="">Nombre de Usuario</label>
+                <input type="text" name="user">
+
+                <label for="">Contraseña</label>
+                <input type="password" name="password">
+                
+                <input type="submit" value="Iniciar Sesión">
+            </form>
+            <a href="./register.php">¿Aún no estás registrado? Regístrate ahora!</a>
+        </section>
+    </main>
 </body>
 </html>
