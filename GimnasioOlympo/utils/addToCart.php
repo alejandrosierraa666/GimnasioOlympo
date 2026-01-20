3ee1e7c6f29b51bf632f712e0f9c8e0b87a1be2c
Alejandro Sierra <sierralagarteraalejandro2003@gmail.com>
2026-01-20 13:02:13 +0100

    addToCart refactorized

--- GimnasioOlympo/utils/addToCart.php
+++ GimnasioOlympo/utils/addToCart.php
@@ -1,15 +1,8 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['id'])) {
-    if (!isset($_COOKIE['cart'])) {
-        $list = [];
-        array_push($list, $_POST['id']);
-        setcookie('cart', json_encode($list), time() + 60 * 24 * 30, "/");
-    } else {
-        $list = json_decode($_COOKIE['cart'], true);
-        array_push($list, $_POST["id"]);
-        setcookie('cart', json_encode($list), time() + 60 * 24 * 30, "/");
-        print_r($list);
-    }
-
+    !isset($_COOKIE['cart']) ? $list = [] : $list = json_decode($_COOKIE['cart'], true);
+    array_push($list, $_POST["id"]);
+    setcookie('cart', json_encode($list), time() + 60 * 24 * 30, "/");
+    
     header('Location: ../pages/products.php');
 }
