99fcae1e8e2779df7302a346b40d89c8b4a474b4
Alejandro Sierra <sierralagarteraalejandro2003@gmail.com>
2026-01-21 13:35:53 +0100

    Some css changed

--- GimnasioOlympo/index.php
+++ GimnasioOlympo/index.php
@@ -10,19 +10,20 @@ include('./utils/checkSession.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Gimnasio Olympo</title>
 </head>
 
 <body>
-    <header>
+    <header class="header">
         <main class="header__container">
-            <img src="././assets/images/logo.jpg" alt="">
-            <nav>
-                <ul>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
+            <img src="./assets/images/logo.png" alt="" class="logo">
+            <nav class="nav">
+                <ul class="nav__list">
+                    <li class="list__item"><a class="list__link active" href="">Inicio</a></li>
+                    <li class="list__item"><a class="list__link" href="">Productos</a></li>
+                    <li class="list__item"><a class="list__link" href="">Mi Perfil</a></li>
+                    <li class="list__item"><a class="list__link" href="">Sobre Nosotros</a></li>
                 </ul>
             </nav>
         </main>
