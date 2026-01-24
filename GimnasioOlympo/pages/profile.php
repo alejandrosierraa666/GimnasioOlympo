8d8726064e20011d07501d6afc76422e5429b5a6
Alejandro Sierra <sierralagarteraalejandro2003@gmail.com>
2026-01-24 11:51:38 +0100

    addUser logic implemmented

--- GimnasioOlympo/pages/profile.php
+++ GimnasioOlympo/pages/profile.php
@@ -11,6 +11,7 @@ include('./../db/db.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./../css/style.css">
     <link rel="stylesheet" href="./../css/profile.css">
+    <link rel="stylesheet" href="./../css/register.css">
     <title>Profile</title>
 </head>
 
@@ -27,25 +28,30 @@ include('./../db/db.php');
     ?>
 
     <?php if ($_SESSION['role'] == 'admin'){ ?>
-        <div style="border:1px solid red">
-            <p>Esta capa solo la van a poder ver los admins</p>
-
-            <section>
-                <article>
+        <div>
+            <section class="admin__options">
+                <article class="option">
                     <h2>Gestión de Usuarios</h2>
                     <p>Accede a la información de los usuarios registrados.</p>
-                    <button id="btn-users">Ver Usuarios</button>
+                    <button id="btn-users" class="admin__button">Ver Usuarios</button>
                 </article>
 
-                <article>
+                <article class="option">
                     <h2>Gestión de Productos</h2>
                     <p>Añade, edita o elimina productos del catálogo.</p>
-                    <button>Gestionar Productos</button>
+                    <button class="admin__button">Gestionar Productos</button>
+                </article>
+                <article class="option">
+                    <h2>Agregar Nuevo Usuario</h2>
+                    <p>Crea una nueva cuenta de usuario para el gimnasio.</p>
+                <button class="admin__button--add" id="addUser">Añadir un nuevo usuario</button>
                 </article>
             </section>
 
 
-            <section style="display: none; margin-top:20px;" id="users">
+            <section id="users" style="display: none; padding-bottom:40px; flex-direction:column; align-items:center; margin:50px auto; gap:1rem; box-shadow: 0 0 10px rgba(0, 0, 0, 0.35); padding-top:10px; width:80%; border-radius:10px;">
+                <h2>Usuarios Registrados</h2>
+                <section style="display: flex; margin-top:20px; justify-content:center; gap:1rem;">
                 <?php
                 try {
                     $stmt = $db->prepare('select * from users');
@@ -57,25 +63,47 @@ include('./../db/db.php');
 
                         !empty($user['profile_picture']) && !isset($user['profile_picture']) ? $route = $user['profile_picture'] : $route = './../assets/images/default_picture.webp';
 
-                        echo "<article style='border:1px solid black; margin:10px; padding:10px; display:flex; flex-direction:column; align-items:center; width:500px;'>";
-                        echo "<img src='" . $route . "' style='width:100%; height:400px; border-radius:50%;'>";
+                        echo "<article style='border:1px solid #ff6600; padding:10px; display:flex; flex-direction:column; align-items:center; width:400px; border-radius:10px;'>";
+                        echo "<img src='" . $route . "' style='width:50%; height:200px; border-radius:50%;'>";
                         echo "<p>Nombre de Usuario: " . $user['user'] . "</p>";
                         echo "<p>Nombre: " . $user['name'] . "</p>";
                         echo "<p>Apellidos: " . $user['last_name'] . "</p>";
-                        echo "<p>Suscrito hasta: " . $user['expiration_date'] . "</p>";
-                        echo "<button>Borrar Usuario</button>";
-                        echo "<button>Renovar el bono</button>";
+                        echo "<p>Registrado el : " . $user['expiration_date'] . "</p>";
+                        echo "<form action='./../utils/deleteUser.php?user=" . $user['user'] . "' method='POST'>";
+                        echo "<button class='delete__button'>Borrar Usuario</button>";
+                        echo "</form>";
+                        echo "<button class='renew__button'>Renovar el bono</button>";
                         echo "</article>";
                     }
                 } catch (Exception $e) {
                     echo $e->getMessage();
                 }
                 ?>
-
+                </section>
                 
-                <button>Añadir un nuevo usuario</button>
             </section>
 
+            <section id="addUserCapa" style="display: none; padding-bottom:40px; flex-direction:column; align-items:center; margin:50px auto; gap:1rem; box-shadow: 0 0 10px rgba(0, 0, 0, 0.35); padding:20px; width:50%; border-radius:10px;">
+                <form action="./../utils/addUser.php" method="POST" class="register__form">
+                    <h2>Añadir Nuevo Usuario</h2>
+                    <label for="user">Nombre de Usuario:</label>
+                    <input type="text" id="user" name="user" required>
+
+                    <label for="name">Nombre:</label>
+                    <input type="text" id="name" name="name" required>
+
+                    <label for="last_name">Apellidos:</label>
+                    <input type="text" id="last_name" name="last_name" required>
+
+                    <label for="password">Contraseña:</label>
+                    <input type="password" id="password" name="password" required>
+
+                    <label for="expiration_date">Fecha de Expiración (YYYY-MM-DD):</label>
+                    <input type="date" id="expiration_date" name="expiration_date" required>
+
+                    <button type="submit" class="admin__button--add">Crear Usuario</button>
+                </form>
+            </section>
         </div>
     <?php }?>
 </body>
@@ -85,14 +113,44 @@ include('./../db/db.php');
 <script>
     let btnUsers = document.getElementById('btn-users');
     let users = document.getElementById('users');
+    let btnAddUser = document.getElementById('addUser');
+    let addUserCapa = document.getElementById('addUserCapa');
+
+    function seeUsers() {
+        if (users.style.display === 'none') {
+            users.style.display = 'flex';
+            btnUsers.textContent = 'Ocultar Usuarios';
+        }else {
+            hideUsers();
+        }
+    }
+
+    function hideUsers() {
+        users.style.display = 'none';
+        btnUsers.textContent = 'Ver Usuarios';
+    }
+
+    function hideAddUser() {
+        addUserCapa.style.display = 'none';
+        btnAddUser.textContent = 'Añadir un nuevo usuario';
+    }
+
+    function seeAddUser() {
+        if (addUserCapa.style.display === 'none') {
+            addUserCapa.style.display = 'block';
+            btnAddUser.textContent = 'Ocultar formulario';
+        } else {
+            hideAddUser();
+        }
+    }
 
     btnUsers.addEventListener('click', () => {
-        if (users.style.display === 'none') {
-            users.style.display = 'grid';
-            btnUsers.textContent = 'Ocultar Usuarios';
-        } else {
-            users.style.display = 'none';
-            btnUsers.textContent = 'Ver Usuarios';
-        }
+        seeUsers()
+        hideAddUser()
+    });
+
+    btnAddUser.addEventListener('click', () => {
+        seeAddUser()
+        hideUsers()
     });
 </script>
