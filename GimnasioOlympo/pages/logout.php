161df0778b64ea41d6702fd796806a53b31fd03e
Alejandro Sierra <sierralagarteraalejandro2003@gmail.com>
2026-01-26 17:13:46 +0100

    Session time updated

--- GimnasioOlympo/pages/logout.php
+++ GimnasioOlympo/pages/logout.php
@@ -4,4 +4,5 @@ session_unset();
 session_destroy();
 
 setcookie('cart', 0, time() - 3600, '/');
+setcookie('lastConnection', '', time() - 3600, '/');
 header('Location: ./login.php');
--- GimnasioOlympo/utils/checkSession.php
+++ GimnasioOlympo/utils/checkSession.php
@@ -1,12 +1,28 @@
 <?php
-    session_start();
-    if(!isset($_SESSION['user'])){
-        if(str_contains($_SERVER['PHP_SELF'], '/pages/')){
+session_start();
+if (!isset($_SESSION['user'])) {
+    if (str_contains($_SERVER['PHP_SELF'], '/pages/')) {
+        header('Location: ./login.php');
+        exit();
+    }
+
+    header('Location: ./pages/login.php');
+    exit();
+}
+
+if (isset($_COOKIE['lastConnection']) && isset($_SESSION['role']) && $_SESSION['role'] != 'admin') {
+    if (strtotime($_COOKIE['lastConnection']) < time() - 24 * 60 * 60) {
+        session_unset();
+        session_destroy();
+
+        if (str_contains($_SERVER['PHP_SELF'], '/pages/')) {
             header('Location: ./login.php');
             exit();
         }
 
         header('Location: ./pages/login.php');
         exit();
+    } else {
+        setcookie('lastConnection', date("Y-m-d H:i:s"), time() + 24 * 60 * 365, '/');
     }
-?>
+}
