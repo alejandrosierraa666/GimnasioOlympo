97d195a5c1635339279e686ccd1bf924a2dc6982
carlos <carlos.gomezsanchez@riberadeltajo.es>
2026-01-26 16:54:53 +0100

    fixed styles on products page

--- GimnasioOlympo/pages/products.php
+++ GimnasioOlympo/pages/products.php
@@ -9,8 +9,8 @@ include('./../db/db.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/products.css">
+    <link rel="stylesheet" href="./../css/style.css">
+    <link rel="stylesheet" href="./../css/products.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
 </head>
 
